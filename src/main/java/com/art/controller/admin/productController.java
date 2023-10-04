9f06501eaf81092a1dea5c4109fc8b9cc4608160
nguyentc148 <tranchingyen.c2bt@gmail.com>
2023-10-04 19:11:20 +0700

    hello anh em

--- src/main/java/com/art/controller/admin/productController.java
+++ src/main/java/com/art/controller/admin/productController.java
@@ -1,17 +1,36 @@
 package com.art.controller.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.art.DAO.Product.CategoryDAO;
+import com.art.DAO.Product.ManufacturerDAO;
+import com.art.Entities.Product.Category;
+import com.art.Entities.Product.Manufacturer;
+
 @Controller
 @RequestMapping("/admin")
 public class productController {
+	@Autowired
+	CategoryDAO caDAO;
+	@Autowired
+	ManufacturerDAO mnDAO;
+
 	@GetMapping("/product")
 	public String product(Model model) {
+		List<Category> listCate = caDAO.findAll();
+		List<Manufacturer> listManu = mnDAO.findAll();
+		
 		model.addAttribute("views", "product-form");
 		model.addAttribute("title", "Quản lí sản phẩm");
+		
+		model.addAttribute("categories", listCate);
+		model.addAttribute("manufacturer", listManu);
 		return "admin/index";
 	}
 }
